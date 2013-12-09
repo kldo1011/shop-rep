c0ee436f40ce35ddb059f5399c691d2143dd4ed7
Dominik <kldo1011@hs-karlsruhe.de>
2013-12-09 16:49:22 +0100

    Bestellung , position hinzugefügt

---
+++ shop/src/main/java/bestellverwaltung/domain/Position.java
@@ -0,0 +1,91 @@
+package bestellverwaltung.domain;
+
+import java.io.Serializable;
+import java.net.URI;
+
+import javax.persistence.Basic;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import artikelverwaltung.domain.*;
+
+
+@XmlRootElement
+public class Position implements Serializable {
+
+                private static final long serialVersionUID = 2640612708567145520L;
+
+                @Id
+                @GeneratedValue
+                @Basic(optional = false)
+        private long id;
+                
+        private Integer bestellid;
+        
+        @XmlTransient
+        @Valid
+        private Artikel artikel;
+        
+        @NotNull(message = "{bestellverwaltung.position.anzahl.notNull}")
+        @Min(value = 1, message = "{bestellverwaltung.position.anzahl.min}")
+        private Integer anzahl;
+        
+        private URI ArtikelUri;
+        
+        public URI getArtikelUri() {
+                        return ArtikelUri;
+                }
+
+                public void setArtikelUri(URI artikelUri) {
+                        ArtikelUri = artikelUri;
+                }
+
+                public long getId() {
+                return id;
+        }
+        
+        public void setId(long id) {
+                this.id = id;
+        }
+        
+        public int getBestellid() {
+                return bestellid;
+        }
+        
+        public void setBestellid(Integer bestellid) {
+                this.bestellid = bestellid;
+        }
+        
+        public Artikel getArtikel() {
+                return artikel;
+        }
+        
+        public void setArtikel(Artikel artikel) {
+                this.artikel = artikel;
+        }
+        
+        public int getAnzahl() {
+                return anzahl;
+        }
+        
+        public void setAnzahl(Integer anzahl) {
+                this.anzahl = anzahl;
+        }
+        
+        public Position() {
+                super();
+        }
+        
+                @Override
+                public String toString() {
+                        return "Position [id=" + id + ", bestellid=" + bestellid
+                                        + ", artikel=" + artikel + ", anzahl=" + anzahl + "]";
+                }
+
+
+}
