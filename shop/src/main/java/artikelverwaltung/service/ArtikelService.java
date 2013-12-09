9659273149125890e104d11fbac872682e6bfbc0
Adam Gabriel <gaad1011(at)hs-karlsruhe.de>
2013-12-09 16:54:10 +0100

    Artikel fertig

--- shop/src/main/java/artikelverwaltung/service/ArtikelService.java
+++ shop/src/main/java/artikelverwaltung/service/ArtikelService.java
@@ -1,5 +1,43 @@
 package artikelverwaltung.service;
 
-public class ArtikelService {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.enterprise.context.Dependent;
+import javax.validation.constraints.NotNull;
+
+import artikelverwaltung.domain.Artikel;
+import util.interceptor.Log;
+import util.Mock;
+
+@Dependent
+@Log
+public class ArtikelService implements Serializable {
+        private static final long serialVersionUID = -5105686816948437276L;
+        
+        private static final int MAX_ID = 10;
+
+        @NotNull(message = "{artikel.notFound.id}")
+        public Artikel findArtikelById(int id) {
+                // TODO id pruefen                
+                if (id > MAX_ID)
+                        return null;
+                // TODO Datenbanzugriffsschicht statt Mock
+                return Mock.findArtikelById(id);
+        }
+        
+        public List<Artikel> findAllArtikel() {
+        
+                return Mock.findAllArtikel();
+        }
+        
+        public Artikel createArtikel(Artikel artikel) {
+                
+                return Mock.createArtikel(artikel);
+        }
+        
+        public void updateArtikel(Artikel artikel) {
+                
+                Mock.updateArtikel(artikel);
+        }
 }
