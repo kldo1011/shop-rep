4f6819c8cb40135a02d89633f8513ac470291e4a
Sebastian Bönig <sboenig(at)web.de>
2013-12-09 17:20:07 +0100

    SessionProducer

---
+++ shop/src/main/java/util/mail/SessionProducer.java
@@ -0,0 +1,15 @@
+package util.mail;
+
+/*import javax.annotation.Resource;
+import javax.enterprise.inject.Produces;
+import javax.mail.Session;
+*/
+/**
+* @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
+*/
+/*
+public class SessionProducer {
+        @Resource(lookup = "java:jboss/mail/Default")
+        @Produces
+        private Session session;
+}*/
