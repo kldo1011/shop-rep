2bcfe50c44e87a207a9631d8ab1afde6b210195e
Dominik <kldo1011@hs-karlsruhe.de>
2013-12-09 16:55:30 +0100

    Bestellung service

---
+++ shop/src/main/java/bestellverwaltung/service/BestellungService.java
@@ -0,0 +1,53 @@
+package bestellverwaltung.service;
+import java.util.List;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
+import bestellverwaltung.domain.Bestellung;
+import bestellverwaltung.domain.Position;
+import kundenverwaltung.domain.AbstractKunde;
+import util.Mock;
+
+@Dependent
+public class BestellungService {
+                
+                
+                @Inject
+                @NeueBestellung
+                private transient Event<Bestellung> event;
+                
+                public Bestellung findBestellungById(Long id) {
+                        // TODO Datenbanzugriffsschicht statt Mock
+                        return Mock.findBestellungById(id);
+                }
+
+                public List<Bestellung> findBestellungenByKunde(AbstractKunde kunde) {
+                        // TODO Datenbanzugriffsschicht statt Mock
+                        return Mock.findBestellungenByKunde(kunde);
+                }
+                //TODO eigentlich noch AbstractKunde kunde und Locale local als parameter, verstehe nicht warum
+                public Bestellung createBestellung(Bestellung bestellung) {
+                        // TODO Datenbanzugriffsschicht statt Mock
+                        bestellung = Mock.createBestellung(bestellung);
+                        event.fire(bestellung);
+                        
+                        return bestellung;
+                }
+                
+                public Position createPosition(Position pos) {
+                        //TODO Datenbankzugriffsschicht statt mock
+                        pos = Mock.createPositionen(pos);
+                        return pos;
+                }
+                public List<Position> findallPositionen(int id) {
+                        List<Position> erg = Mock.findAllPositionen(id);
+                        return erg;
+                        
+                }
+
+                public Position updatePosition(Position pos) {
+                        pos = Mock.updatePosition(pos);
+                        return pos;
+                }
+                
+}
